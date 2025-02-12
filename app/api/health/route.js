d32b5c719780f6a10f51932a3cca23937afc0e4b
Antonio Queb <antonioqueb@alphaqueb.online>
2025-02-12 13:59:05 -0600

    Up

--- app/api/health/route.js
+++ app/api/health/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 
-// Función para formatear el tiempo de actividad
+// Desactiva el caching para obtener valores en tiempo real
+export const dynamic = "force-dynamic";
+
+// Función para formatear el tiempo de actividad en formato hh:mm:ss
 function formatUptime(seconds) {
   const h = Math.floor(seconds / 3600);
   const m = Math.floor((seconds % 3600) / 60);
@@ -13,8 +16,8 @@ export async function GET() {
   return NextResponse.json(
     {
       status: "ok",
-      uptime: formatUptime(process.uptime()),
-      timestamp: new Date().toLocaleString(), // Formato local
+      uptime: formatUptime(process.uptime()), // Tiempo real
+      timestamp: new Date().toLocaleString(), // Formato legible
     },
     { status: 200 }
   );
