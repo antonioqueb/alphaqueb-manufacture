acae0527528c6915eee8d8396a2ed8a2ef3d5a4b
Antonio Queb <antonioqueb@alphaqueb.online>
2025-02-16 10:32:28 -0600

    Up

--- app/servicios/scada/page.jsx
+++ app/servicios/scada/page.jsx
@@ -33,24 +33,23 @@ const App = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Hero Section con imagen */}
         <section className="relative dark:bg-zinc-900 py-10 md:py-28 rounded-3xl overflow-hidden mb-16">
-          <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center px-6">
+          <div className="max-w-6xl mx-auto grid sm:grid-cols-1 md:grid-cols-2 gap-12 items-center px-6">
             <div className="space-y-6">
-              <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-custom-orange to-orange-700 bg-clip-text text-transparent dark:from-custom-orange dark:to-zinc-400 leading-tight">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold bg-gradient-to-r from-custom-orange to-orange-700 bg-clip-text text-transparent dark:from-custom-orange dark:to-zinc-400 leading-tight">
                 {COPY.title}
               </h1>
-              <p className="text-lg text-zinc-600 dark:text-zinc-300 mb-8">
+              <p className="text-base sm:text-lg text-zinc-600 dark:text-zinc-300 mb-8">
                 {COPY.description1}
               </p>
-              {/* Botón para ir a la sección de contacto */}
               <Link href="#contacto">
-                <Button variant="default" size="lg" style={{ backgroundColor: '#ff943d', color: '#0d0d0d', margin: '2rem 0rem', padding: '0.75rem 2rem' }}>
+                <Button variant="default" size="lg" className="bg-orange-500 text-black py-3 px-6 rounded-lg">
                   Contáctanos
                 </Button>
               </Link>
             </div>
-            <div className="relative h-96 rounded-2xl overflow-hidden shadow-xl">
+            <div className="relative w-full h-64 sm:h-80 md:h-96 rounded-2xl overflow-hidden shadow-xl">
               <Image
-                src="/512px-thumbnail.jpg"
+                src="/fulfillment.jpg"
                 alt="Digital Transformation"
                 fill
                 className="object-cover object-center"
@@ -60,71 +59,42 @@ const App = () => {
             </div>
           </div>
         </section>
+
         {/* Sección de Ventajas */}
-        <section className="grid lg:grid-cols-2 gap-8 mb-10">
-          <div className="bg-zinc-50 dark:bg-zinc-900/50 p-8 rounded-lg shadow-md col-span-1">
-            <ul className="space-y-8">
-              {COPY.advantages.slice(0, Math.ceil(COPY.advantages.length / 2)).map((advantage, index) => {
-                const [key, value] = advantage.split(": ");
-                const icons = [<Zap key="0" />, <BarChart key="1" />, <Clock key="2" />, 
-                              <Terminal key="3" />, <Users key="4" />, <Rocket key="5" />];
-                
-                return (
-                  <li key={index} className="flex items-start group">
-                    <div className="flex-shrink-0 mt-1 mr-6 text-custom-orange">
-                      <div className="w-10 h-10 bg-custom-orange/20 rounded-lg flex items-center justify-center p-2">
-                        {React.cloneElement(icons[index], { className: 'w-6 h-6' })}
-                      </div>
-                    </div>
-                    <div className="flex-1">
-                      <h4 className="font-semibold text-lg text-zinc-900 dark:text-zinc-100 mb-2">
-                        {key}
-                      </h4>
-                      <p className="text-zinc-600 dark:text-zinc-300 leading-relaxed">
-                        {value}
-                      </p>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="bg-zinc-50 dark:bg-zinc-900/50 p-8 rounded-lg shadow-md col-span-1">
-            <ul className="space-y-8">
-              {COPY.advantages.slice(Math.ceil(COPY.advantages.length / 2)).map((advantage, index) => {
-                const [key, value] = advantage.split(": ");
-                const icons = [<Zap key="0" />, <BarChart key="1" />, <Clock key="2" />, 
-                              <Terminal key="3" />, <Users key="4" />, <Rocket key="5" />];
-                
-                return (
-                  <li key={index + Math.ceil(COPY.advantages.length / 2)} className="flex items-start group">
-                    <div className="flex-shrink-0 mt-1 mr-6 text-custom-orange">
-                      <div className="w-10 h-10 bg-custom-orange/20 rounded-lg flex items-center justify-center p-2">
-                        {React.cloneElement(icons[index + Math.ceil(COPY.advantages.length / 2)], { className: 'w-6 h-6' })}
-                      </div>
-                    </div>
-                    <div className="flex-1">
-                      <h4 className="font-semibold text-lg text-zinc-900 dark:text-zinc-100 mb-2">
-                        {key}
-                      </h4>
-                      <p className="text-zinc-600 dark:text-zinc-300 leading-relaxed">
-                        {value}
-                      </p>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+        <section className="grid sm:grid-cols-1 lg:grid-cols-2 gap-8 mb-10">
+          {COPY.advantages.map((advantage, index) => {
+            const [key, value] = advantage.split(": ");
+            const icons = [<Zap key="0" />, <BarChart key="1" />, <Clock key="2" />, 
+                            <Terminal key="3" />, <Users key="4" />, <Rocket key="5" />];
+            
+            return (
+              <div key={index} className="bg-zinc-50 dark:bg-zinc-900/50 p-6 sm:p-8 rounded-lg shadow-md flex flex-col md:flex-row items-start">
+                <div className="flex-shrink-0 mt-1 mr-4 text-custom-orange">
+                  <div className="w-10 h-10 bg-custom-orange/20 rounded-lg flex items-center justify-center p-2">
+                    {React.cloneElement(icons[index % icons.length], { className: 'w-6 h-6' })}
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <h4 className="font-semibold text-lg text-zinc-900 dark:text-zinc-100 mb-2">
+                    {key}
+                  </h4>
+                  <p className="text-zinc-600 dark:text-zinc-300 leading-relaxed">
+                    {value}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </section>
        
         {/* Sección de Contacto */}
-        <section id="contacto" className="border-t pt-5 border-zinc-200 dark:border-zinc-700"> {/* Añadimos el id "contacto" */}
+        <section id="contacto" className="border-t pt-5 border-zinc-200 dark:border-zinc-700"> 
           <ContactSection />
         </section>
       </div>
     </TooltipProvider>
   );
 };
+
 
 export default App;
