d053a93a9798804659bd53b59afcb4a11a306f74
Antonio Queb <cardstillo@gmail.com>
2026-03-14 20:24:26 -0600

    style: Add dark class to the root HTML element.

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background font-sans mx-6 xl:mx-8 2xl:mx-16`}
       >
